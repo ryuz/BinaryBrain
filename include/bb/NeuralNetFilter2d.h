55de73fa25c6d249f660a1dd56c94904377f03cd
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2019-01-04 14:50:32 +0900

    update

--- include/bb/NeuralNetFilter2d.h
+++ include/bb/NeuralNetFilter2d.h
@@ -23,12 +23,12 @@ class NeuralNetFilter2d : public NeuralNetLayerBuf<T>
 public:
 	virtual int GetInputChannel(void)  const { return 1; }
 	virtual int GetInputHeight(void)   const { return 1; }
-	virtual int GetInputWidth(void)    const { return (int)GetInputNodeSize(); }
+	virtual int GetInputWidth(void)    const { return (int)this->GetInputNodeSize(); }
 	virtual int GetFilterHeight(void)  const { return 1; }
 	virtual int GetFilterWidth(void)   const { return 1; }
 	virtual int GetOutputChannel(void) const { return 1; }
 	virtual int GetOutputHeight(void)  const { return 1; }
-	virtual int GetOutputWidth(void)   const { return (int)GetOutputNodeSize(); }
+	virtual int GetOutputWidth(void)   const { return (int)this->GetOutputNodeSize(); }
 };
 
 
