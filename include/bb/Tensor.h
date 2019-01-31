01f4f7ff7abe073160bb639083d24e1ec280d994
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2019-01-31 19:26:57 +0900

    update

--- include/bb/Tensor.h
+++ include/bb/Tensor.h
@@ -318,27 +318,27 @@ template<typename T>
 inline Tensor_<T> operator+(const Tensor_<T> &src0, Tensor_<T> const &src1)
 {
     BB_ASSERT(src0.m_size == src1.m_size);
-    Tensor_<T>  dst(op0.m_shape);
+    Tensor_<T>  dst(src0.m_shape);
     auto op3 = Memory::GetOp3Ptr(dst.m_mem, src0.m_mem, src1.m_mem);
     Tensor_Scalar_add_ex<T>((T *)op3.dst.GetPtr(), (const T *)op3.src0.GetPtr(), (const T *)op3.src1.GetPtr(), (T)1, (T)1, (T)0, dst.m_size);
     return dst;
 }
 
-template<typename T>
-Tensor_<T> operator+(const Tensor_<T>& src0, T src1);
+//template<typename T>
+//Tensor_<T> operator+(const Tensor_<T>& src0, T src1);
 
-/*
+
 template<typename T>
 Tensor_<T> operator+(const Tensor_<T>& src0, T src1)
 {
    BB_ASSERT(src0.m_size == src1.m_size);
    Tensor_<T>  dst(op0.m_shape);
- //  auto op3 = Memory::GetOp3Ptr(dst.m_mem, dst.m_mem, src0.m_mem);
- //   Tensor_Scalar_add_ex<T>((T *)op3.dst.GetPtr(), (const T *)op3.src0.GetPtr(), (const T *)op3.src1.GetPtr(), (T)1, (T)0, src1, dst.m_size);
- //   return dst;
-    return Tensor_<T>();
+   auto op3 = Memory::GetOp3Ptr(dst.m_mem, dst.m_mem, src0.m_mem);
+   Tensor_Scalar_add_ex<T>((T *)op3.dst.GetPtr(), (const T *)op3.src0.GetPtr(), (const T *)op3.src1.GetPtr(), (T)1, (T)0, src1, dst.m_size);
+   return dst;
+//    return Tensor_<T>();
 }
-*/
+
 
 template<typename T>
 Tensor_<T> operator+(T src0, Tensor_<T> const &src1)
