4d1e40b52f2259ef98bc0149e166c7483d25949d
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2019-02-17 10:39:13 +0900

    update

--- include/bb/Variables.h
+++ include/bb/Variables.h
@@ -41,17 +41,79 @@ public:
 
     ~Variables(){}
 
-    Variables& operator=(Variables const &v)
+    index_t GetSize(void) const
     {
-        m_tensors = v.m_tensors;
-        return *this;
+        return (index_t)m_tensors.size();
     }
 
-    Variables& operator+=(Variables const &v)
+    std::vector<int> GetTypes(void) const
+    {
+        std::vector<int> types;
+        for ( auto& t : m_tensors ) {
+            types.push_back(t.GetType());
+        }
+    }
+
+    std::vector<indices_t> GetShapes(void) const
+    {
+        std::vector<indices_t> shapes;
+        for ( auto& t : m_tensors ) {
+            shapes.push_back(t.GetShape());
+        }
+    }
+    
+    void PushBack(Tensor const &t)
+    {
+        m_tensors.push_back(t);
+    }
+    
+
+    // access operators
+    Tensor const &operator[](index_t index) const
+    {
+        BB_DEBUG_ASSERT(index >= 0 && index < GetSize());
+        return m_tensors[index];
+    }
+
+    Tensor &operator[](index_t index)
+    {
+        BB_DEBUG_ASSERT(index >= 0 && index < GetSize());
+        return m_tensors[index];
+    }
+
+
+    // arithmetic operator
+    Variables &operator=(Variables const &src)
+    {
+        m_tensors = src.m_tensors;
+        return *this;
+    }
+    
+    template<typename Tp>
+    Variables &operator=(Tp src)
     {
         BB_ASSERT(m_tensors.size() == v.m_tensors.size());
         for ( size_t i = 0; i < m_tensors.size(); ++i ) {
-            m_tensors[i] += v.m_tensors[i];
+            m_tensors[i] = src;
+        }
+        return *this;
+    }
+
+    Variables &operator+=(Variables const &src)
+    {
+        BB_ASSERT(m_tensors.size() == src.m_tensors.size());
+        for ( size_t i = 0; i < m_tensors.size(); ++i ) {
+            m_tensors[i] += src.m_tensors[i];
+        }
+        return *this;
+    }
+
+    template<typename Tp>
+    Variables &operator+=(Tp src)
+    {
+        BB_ASSERT(m_tensors.size() == v.m_tensors.size());
+        for ( size_t i = 0; i < m_tensors.size(); ++i ) {
+            m_tensors[i] += src;
         }
         return *this;
     }
