05b03c286375d62bd399d8f44557f3ec12ea4d26
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2019-03-09 18:37:38 +0900

    add HorizontalMeanVar

---
+++ cuda/HorizontalMeanVar.cu
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <chrono>
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include "bbcu/bbcu.h"
+#include "bbcu/bbcu_util.h"
+
+
+// kernel
+__global__ void kernel_fp32_HorizontalMeanVar(
+	const float*	src,
+	float*			mean,
+	float*			variance,
+	int				frame_size,
+	int				frame_stride)
+{
+	extern __shared__   float	buf[];
+	
+	// 初期化
+	int node  = blockIdx.x;
+	int frame = threadIdx.x;
+	int frame_step = blockDim.x;
+	
+	
+	// カハンの加算アルゴリズム(Kahan summation algorithm)
+	float s1 = 0, c1 = 0, y1, t1;
+	float s1 = 0, c1 = 0, y1, t1;
+	const float* src_ptr = &src[frame_stride * node];
+	while ( frame < frame_size ) {
+		float x = src_ptr[frame];
+		
+		y1 = x - c1;
+		t1 = s1 + y1;
+		c1 += (t1 - s1) - y1;
+		
+		y2 = x - c2;
+		t2 = s2 + y2;
+		c2 += (t2 - s2) - y2;
+		
+		frame += frame_step;
+	}
+	
+	float* buf1 = &buf[0];
+	float* buf2 = &buf[blockDim.x];
+	
+	buf1[threadIdx.x] = s1;
+	buf2[threadIdx.x] = s2;
+	
+	__syncthreads();
+	
+	// スレッド間集計
+	int comb = 1;
+	while (comb < frame_size) {
+		int next = comb * 2;
+		int mask = next - 1;
+		if ((threadIdx.x & mask) == 0) {
+			buf1[threadIdx.x] += buf1[threadIdx.x + comb];
+			buf2[threadIdx.x] += buf2[threadIdx.x + comb];
+		}
+		comb = next;
+		__syncthreads();
+	}
+	
+	float m = buf1[0] / frame_size;
+	float v = (buf2[0] / frame_size) - (m * m);
+	
+	mean[node]     = m;
+	variance[node] = v;
+}
+
+
+int bbcu_fp32_HorizontalMeanVar
+(
+	const float*	dev_src,
+	float*			dev_mean,
+	float*			dev_var,
+	int				node_size,
+	int				frame_size,
+	int				frame_stride,
+	cudaStream_t	streamId
+)
+{
+	BBCU_DEBUG_ASSERT(bbcu_IsDeviceAvailable());
+	
+	int		unit_x = 512;
+	
+	dim3	grid(node_size);
+	dim3	block(unit_x);
+	
+	kernel_fp32_HorizontalMeanVar << <grid, block, 2 * unit_x * sizeof(float), streamId >> > (
+		dev_src,
+		dev_dst,
+		frame_size,
+		frame_stride);
+	
+	cudaError_t cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		exit(1);
+		return 1;
+	}
+
+	return 0;
+}
+
+
