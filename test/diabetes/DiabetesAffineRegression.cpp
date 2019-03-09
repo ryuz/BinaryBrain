ab2592d383416e471df96291e5a02033ed12e3e9
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2019-03-09 21:44:13 +0900

    create diabetes

---
+++ test/diabetes/DiabetesAffineRegression.cpp
@@ -0,0 +1,104 @@
+﻿// --------------------------------------------------------------------------
+//  BinaryBrain  -- binary network evaluation platform
+//   MNIST sample
+//
+//                                     Copyright (C) 2018 by Ryuji Fuchikami
+// --------------------------------------------------------------------------
+
+
+#include <iostream>
+#include <fstream>
+#include <numeric>
+#include <random>
+#include <chrono>
+
+#include "bb/DenseAffine.h"
+#include "bb/MicroMlp.h"
+#include "bb/BatchNormalization.h"
+#include "bb/ReLU.h"
+#include "bb/AccuracyMeanSquaredError.h"
+#include "bb/LossMeanSquaredError.h"
+#include "bb/OptimizerAdam.h"
+#include "bb/OptimizerSgd.h"
+#include "bb/LoadMnist.h"
+#include "bb/ShuffleSet.h"
+#include "bb/Utility.h"
+#include "bb/Sequential.h"
+#include "bb/Runner.h"
+#include "bb/RealToBinary.h"
+#include "bb/BinaryToReal.h"
+
+
+template<typename T=float>
+bb::TrainData<T> LoadDiabetes(int num_train=400)
+{
+	const int n = 442;
+
+	std::ifstream ifs_x("diabetes_data.txt");
+	std::ifstream ifs_t("diabetes_target.txt");
+
+	bb::TrainData<T> td;
+	td.x_shape = bb::indices_t({ 10 });
+	td.t_shape = bb::indices_t({ 1 });
+
+	for (int i = 0; i < num_train; ++i) {
+		std::vector<T> train(10);
+		std::vector<T> target(1);
+		for (int j = 0; j < 10; ++j) {
+			ifs_x >> train[j];
+		}
+		ifs_t >> target[0];
+
+		td.x_train.push_back(train);
+		td.t_train.push_back(target);
+	}
+
+	for (int i = 0; i < n - num_train; ++i) {
+		std::vector<T> train(10);
+		std::vector<T> target(1);
+		for (int j = 0; j < 10; ++j) {
+			ifs_x >> train[j];
+		}
+		ifs_t >> target[0];
+
+		td.x_test.push_back(train);
+		td.t_test.push_back(target);
+	}
+
+	return td;
+}
+
+
+
+void DiabetesAffineRegression(int epoch_size, size_t mini_batch_size)
+{
+  // load diabetes data
+	auto td = LoadDiabetes<>();
+
+    auto net = bb::Sequential::Create();
+	net->Add(bb::DenseAffine<>::Create({ 512 }));
+	net->Add(bb::ReLU<>::Create());
+	net->Add(bb::DenseAffine<>::Create({ 256 }));
+	net->Add(bb::ReLU<>::Create());
+	net->Add(bb::DenseAffine<>::Create({ 1 }));
+
+	net->SetInputShape({ 10 });
+
+    bb::FrameBuffer x(BB_TYPE_FP32, mini_batch_size, { 10 });
+	bb::FrameBuffer t(BB_TYPE_FP32, mini_batch_size, { 1 });
+
+    bb::Runner<float>::create_t runner_create;
+    runner_create.name      = "DiabetesAffineRegression";
+    runner_create.net       = net;
+    runner_create.lossFunc  = bb::LossMeanSquaredError<float>::Create();
+    runner_create.accFunc   = bb::AccuracyMeanSquaredError<float>::Create();
+    runner_create.optimizer = bb::OptimizerSgd<float>::Create(0.00001);
+//  runner_create.optimizer = bb::OptimizerAdam<float>::Create();
+	runner_create.serial_write = false;
+	runner_create.print_progress = false;
+	runner_create.initial_evaluation = true;
+    auto runner = bb::Runner<float>::Create(runner_create);
+
+    runner->Fitting(td, epoch_size, mini_batch_size);
+}
+
---
+++ test/diabetes/main.cpp
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------
+//  BinaryBrain  -- binary network evaluation platform
+//   MNIST sample
+//
+//                                     Copyright (C) 2018 by Ryuji Fuchikami
+// --------------------------------------------------------------------------
+
+
+#include <iostream>
+#include <omp.h>
+
+#include "bb/Manager.h"
+
+void DiabetesAffineRegression(int epoch_size, size_t mini_batch_size);
+void DiabetesRegression(int epoch_size, size_t mini_batch_size);
+
+
+// メイン関数
+int main()
+{
+	omp_set_num_threads(4);
+
+	DiabetesRegression(64, 16);
+	DiabetesAffineRegression(64, 16);
+	
+	return 0;
+}
+
+
+// end of file
