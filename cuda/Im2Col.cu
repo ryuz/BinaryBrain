5040ee067a1fff7da50e74165f05a2bcc83f8b19
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2019-04-16 20:40:15 +0900

    refactoring

--- cuda/Im2Col.cu
+++ cuda/Im2Col.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <chrono>
 
 #include "cuda_runtime.h"
@@ -73,9 +74,12 @@ BBCU_DLL_EXPORT int bbcu_fp32_Im2Col_Forward
     
     int output_frame_size = input_frame_size * output_size;
     
-    int     frame_unit = 16;
-    dim3    grid((output_frame_size + (frame_unit-1))/frame_unit, output_c_size);
+    int     frame_unit = 1024;
+    while ( frame_unit * filter_w_size * filter_h_size > 1024 ) { frame_unit /= 2; }
+    BBCU_ASSERT(frame_unit > 0);
+
     dim3    block(frame_unit, filter_w_size, filter_h_size);
+    dim3    grid((output_frame_size + (frame_unit-1))/frame_unit, output_c_size);
     
     kernal_fp32_Im2Col_Forward<<<grid, block, 0, streamId>>>(
             dev_x_buf,
@@ -202,6 +206,7 @@ __global__ void kernal_fp32_Im2Col_Backward(
             int             input_frame_stride,
             int             input_w_size,
             int             input_h_size,
+            int             input_c_size,
             
             int             output_frame_size,
             int             output_frame_stride,
@@ -216,27 +221,26 @@ __global__ void kernal_fp32_Im2Col_Backward(
     int y = blockDim.y * blockIdx.y + threadIdx.y;
     int c = blockDim.z * blockIdx.z + threadIdx.z;
     
-    float const *output_grad_ptr = &dy_buf[c * filter_h_size * filter_w_size * output_frame_stride];
+    if ( x < input_w_size && y < input_h_size && c < input_c_size ) {
+        float const *dy_ptr = &dy_buf[c * filter_h_size * filter_w_size * output_frame_stride];
 
-    for ( int input_frame = 0; input_frame < input_frame_size; ++input_frame ) {
-        float grad = 0;
-        for (int fy = 0; fy < filter_h_size; ++fy) {
-            int iy = y - fy;
-            if ( iy >= 0 && iy < (input_h_size - filter_h_size + 1)) {
-                for (int fx = 0; fx < filter_w_size; ++fx) {
-                    int ix = x - fx;
-                    if (ix >= 0 && ix < (input_w_size - filter_w_size + 1)) {
-                        int output_frame = (input_frame * output_h_size + iy) * output_w_size + ix;
-                        int output_node  = fy * filter_w_size + fx;
-                        grad += output_grad_ptr[output_node * output_frame_stride + output_frame];
+        for ( int input_frame = 0; input_frame < input_frame_size; ++input_frame ) {
+            float dx = 0;
+            for (int fy = 0; fy < filter_h_size; ++fy) {
+                int iy = y - fy;
+                if ( iy >= 0 && iy < (input_h_size - filter_h_size + 1)) {
+                    for (int fx = 0; fx < filter_w_size; ++fx) {
+                        int ix = x - fx;
+                        if (ix >= 0 && ix < (input_w_size - filter_w_size + 1)) {
+                            int output_frame = (input_frame * output_h_size + iy) * output_w_size + ix;
+                            int output_node  = fy * filter_w_size + fx;
+                            dx += dy_ptr[output_node * output_frame_stride + output_frame];
+                        }
                     }
                 }
             }
+            dx_buf[((c * input_h_size + y) * input_w_size + x) * input_frame_stride + input_frame] = dx;
         }
-//      float*  input_grad_ptr  = &input_grad_buf[c * input_h_size * input_w_size * input_frame_stride];
-//      int input_node  = y * input_w_size + x;
-//      input_grad_ptr[input_node * input_frame_stride + input_frame] = grad;
-        dx_buf[((c * input_h_size + y) * input_w_size + x) * input_frame_stride + input_frame] = grad;
     }
 }
 
@@ -265,8 +269,19 @@ BBCU_DLL_EXPORT int bbcu_fp32_Im2Col_Backward
     
     int output_frame_size = input_frame_size * output_size;
     
-    dim3    grid(input_w_size, input_h_size, 1);
-    dim3    block(1, 1, input_c_size);
+    dim3    block(1024, 1, 1);
+    while ( (int)block.x / 2 >= input_w_size ) { block.x /= 2; block.y *= 2; }
+    while ( (int)block.y / 2 >= input_h_size ) { block.y /= 2; block.z *= 2; }
+    while ( (int)block.z / 2 >= input_c_size ) { block.z /= 2; }
+    block.z = std::min(64, (int)block.z);
+
+    dim3    grid;
+    grid.x = (input_w_size + block.x - 1) / block.x;
+    grid.y = (input_h_size + block.y - 1) / block.y;
+    grid.z = (input_c_size + block.z - 1) / block.z;
+
+//  dim3    grid(input_w_size, input_h_size, 1);
+//  dim3    block(1, 1, input_c_size);
     
     kernal_fp32_Im2Col_Backward<<<grid, block, 0, streamId>>>(
             dev_dy_buf,
@@ -275,6 +290,7 @@ BBCU_DLL_EXPORT int bbcu_fp32_Im2Col_Backward
             input_frame_stride,
             input_w_size,
             input_h_size,
+            input_c_size,
             output_frame_size,
             output_frame_stride,
             output_w_size,
