8cd7b7400aa19fea2b18a4fc0a53a259c9bf3154
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2019-04-20 22:01:39 +0900

    update test

--- test/cifar10/Cifar10StochasticLut6Cnn.cpp
+++ test/cifar10/Cifar10StochasticLut6Cnn.cpp
@@ -37,6 +37,8 @@
 
 #if 0
 
+// 単純版
+
 // CNN with LUT networks
 void Cifar10StochasticLut6Cnn(int epoch_size, int mini_batch_size, int max_run_size, int lut_frame_mux_size, bool binary_mode, bool file_read)
 {
@@ -233,8 +235,12 @@ void Cifar10StochasticLut6Cnn(int epoch_size, int mini_batch_size, int max_run_s
         }
     }
 }
+#endif
 
-#else
+#if 0
+
+// depthwise / pointwise
+
 
 // CNN with LUT networks
 void Cifar10StochasticLut6Cnn(int epoch_size, int mini_batch_size, int max_run_size, int lut_frame_mux_size, bool binary_mode, bool file_read)
@@ -515,6 +521,119 @@ void Cifar10StochasticLut6Cnn(int epoch_size, int mini_batch_size, int max_run_s
 }
 
 #endif
+
+
+
+// BatchNorm実験
+
+// CNN with LUT networks
+void Cifar10StochasticLut6Cnn(int epoch_size, int mini_batch_size, int max_run_size, int lut_frame_mux_size, bool binary_mode, bool file_read)
+{
+    std::string net_name = "Cifar10StochasticLut6Cnn";
+
+  // load cifar-10 data
+#ifdef _DEBUG
+    auto td = bb::LoadCifar10<>::Load(1);
+    std::cout << "!!! debug mode !!!" << std::endl;
+#else
+    auto td = bb::LoadCifar10<>::Load();
+#endif
+
+    // create network
+    auto layer_cnv0_sl0 = bb::StochasticLut6<>::Create(512);
+    auto layer_cnv0_sl1 = bb::StochasticLut6<>::Create(384);
+    auto layer_cnv0_sl2 = bb::StochasticLut6<>::Create(64);
+
+    auto layer_cnv1_sl0 = bb::StochasticLut6<>::Create(512);
+    auto layer_cnv1_sl1 = bb::StochasticLut6<>::Create(384);
+    auto layer_cnv1_sl2 = bb::StochasticLut6<>::Create(64);
+
+    auto layer_cnv2_sl0 = bb::StochasticLut6<>::Create(1024);
+    auto layer_cnv2_sl1 = bb::StochasticLut6<>::Create(768);
+    auto layer_cnv2_sl2 = bb::StochasticLut6<>::Create(128);
+
+    auto layer_cnv3_sl0 = bb::StochasticLut6<>::Create(1024);
+    auto layer_cnv3_sl1 = bb::StochasticLut6<>::Create(768);
+    auto layer_cnv3_sl2 = bb::StochasticLut6<>::Create(64);
+
+    auto layer_sl4 = bb::StochasticLut6<>::Create(2048);
+    auto layer_sl5 = bb::StochasticLut6<>::Create(1024);
+    auto layer_sl6 = bb::StochasticLut6<>::Create(420);
+    auto layer_sl7 = bb::StochasticLut6<>::Create(70);
+
+    {
+        auto cnv0_sub = bb::Sequential::Create();
+        cnv0_sub->Add(layer_cnv0_sl0);
+        cnv0_sub->Add(layer_cnv0_sl1);
+        cnv0_sub->Add(layer_cnv0_sl2);
+
+        auto cnv1_sub = bb::Sequential::Create();
+        cnv1_sub->Add(layer_cnv1_sl0);
+        cnv1_sub->Add(layer_cnv1_sl1);
+        cnv1_sub->Add(layer_cnv1_sl2);
+
+        auto cnv2_sub = bb::Sequential::Create();
+        cnv2_sub->Add(layer_cnv2_sl0);
+        cnv2_sub->Add(layer_cnv2_sl1);
+        cnv2_sub->Add(layer_cnv2_sl2);
+
+        auto cnv3_sub = bb::Sequential::Create();
+        cnv3_sub->Add(layer_cnv3_sl0);
+        cnv3_sub->Add(layer_cnv3_sl1);
+        cnv3_sub->Add(layer_cnv3_sl2);
+        
+        auto net = bb::Sequential::Create();
+        net->Add(bb::BatchNormalization<>::Create());
+        net->Add(bb::LoweringConvolution<>::Create(cnv0_sub, 3, 3));
+        net->Add(bb::BatchNormalization<>::Create());
+        net->Add(bb::LoweringConvolution<>::Create(cnv1_sub, 3, 3));
+        net->Add(bb::MaxPooling<>::Create(2, 2));
+        net->Add(bb::BatchNormalization<>::Create());
+        net->Add(bb::LoweringConvolution<>::Create(cnv2_sub, 3, 3));
+        net->Add(bb::BatchNormalization<>::Create());
+        net->Add(bb::LoweringConvolution<>::Create(cnv3_sub, 3, 3));
+        net->Add(bb::MaxPooling<>::Create(2, 2));
+        net->Add(bb::BatchNormalization<>::Create());
+        net->Add(layer_sl4);
+        net->Add(bb::BatchNormalization<>::Create());
+        net->Add(layer_sl5);
+        net->Add(bb::BatchNormalization<>::Create());
+        net->Add(layer_sl6);
+        net->Add(bb::BatchNormalization<>::Create());
+        net->Add(layer_sl7);
+        net->Add(bb::Reduce<>::Create(td.t_shape));
+        net->SetInputShape(td.x_shape);
+
+        if ( binary_mode ) {
+            std::cout << "binary mode" << std::endl;
+            net->SendCommand("binary true");
+        }
+
+        net->SendCommand("fix_gamma true");
+        net->SendCommand("fix_beta  true");
+        net->SendCommand("set_gamma 0.2");
+        net->SendCommand("set_beta  0.5");
+
+        // print model information
+        net->PrintInfo();
+
+        // run fitting
+        bb::Runner<float>::create_t runner_create;
+        runner_create.name               = net_name;
+        runner_create.net                = net;
+        runner_create.lossFunc           = bb::LossSoftmaxCrossEntropy<float>::Create();
+        runner_create.metricsFunc        = bb::MetricsCategoricalAccuracy<float>::Create();
+        runner_create.optimizer          = bb::OptimizerAdam<float>::Create();
+//      runner_create.optimizer          = bb::OptimizerAdaGrad<float>::Create();
+        runner_create.max_run_size       = max_run_size;    // 実際の1回の実行サイズ
+        runner_create.file_read          = file_read;       // 前の計算結果があれば読み込んで再開するか
+        runner_create.file_write         = true;            // 計算結果をファイルに保存するか
+        runner_create.print_progress     = true;            // 途中結果を表示
+        runner_create.initial_evaluation = file_read;       // ファイルを読んだ場合は最初に評価しておく
+        auto runner = bb::Runner<float>::Create(runner_create);
+        runner->Fitting(td, epoch_size, mini_batch_size);
+    }
+}
 
 
 
