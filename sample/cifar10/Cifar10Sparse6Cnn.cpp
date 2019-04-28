2f0763bffe845bfb25c305ef447acafecff4c67b
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2019-04-28 17:56:04 +0900

    update

--- sample/cifar10/Cifar10Sparse6Cnn.cpp
+++ sample/cifar10/Cifar10Sparse6Cnn.cpp
@@ -50,61 +50,88 @@ void Cifar10Sparse6Cnn(int epoch_size, int mini_batch_size, int max_run_size, bo
 #endif
 
     // create network
-    auto layer_cnv0_sl0 = bb::StochasticLutBn<6>::Create(256);
-    auto layer_cnv0_sl1 = bb::StochasticLutBn<6>::Create(192);
-    auto layer_cnv0_sl2 = bb::StochasticLutBn<6>::Create(32);
-    auto layer_cnv1_sl0 = bb::StochasticLutBn<6>::Create(256);
-    auto layer_cnv1_sl1 = bb::StochasticLutBn<6>::Create(192);
-    auto layer_cnv1_sl2 = bb::StochasticLutBn<6>::Create(32);
-    auto layer_cnv2_sl0 = bb::StochasticLutBn<6>::Create(512);
-    auto layer_cnv2_sl1 = bb::StochasticLutBn<6>::Create(384);
-    auto layer_cnv2_sl2 = bb::StochasticLutBn<6>::Create(64);
-    auto layer_cnv3_sl0 = bb::StochasticLutBn<6>::Create(512);
-    auto layer_cnv3_sl1 = bb::StochasticLutBn<6>::Create(384);
-    auto layer_cnv3_sl2 = bb::StochasticLutBn<6>::Create(64);
-    auto layer_sl4 = bb::StochasticLutBn<6>::Create(2048);
-    auto layer_sl5 = bb::StochasticLutBn<6>::Create(360);
-    auto layer_sl6 = bb::StochasticLutBn<6>::Create(60);
-    auto layer_sl7 = bb::StochasticLutBn<6>::Create(10);
+    auto layer_cnv0_sl0 = bb::StochasticLut6<>::Create(512);
+    auto layer_cnv0_sl1 = bb::StochasticLut6<>::Create(384);
+    auto layer_cnv0_sl2 = bb::StochasticLut6<>::Create(64);
+    auto layer_cnv1_sl0 = bb::StochasticLut6<>::Create(512);
+    auto layer_cnv1_sl1 = bb::StochasticLut6<>::Create(384);
+    auto layer_cnv1_sl2 = bb::StochasticLut6<>::Create(64);
+    auto layer_cnv2_sl0 = bb::StochasticLut6<>::Create(1024);
+    auto layer_cnv2_sl1 = bb::StochasticLut6<>::Create(768);
+    auto layer_cnv2_sl2 = bb::StochasticLut6<>::Create(128);
+    auto layer_cnv3_sl0 = bb::StochasticLut6<>::Create(1024);
+    auto layer_cnv3_sl1 = bb::StochasticLut6<>::Create(768);
+    auto layer_cnv3_sl2 = bb::StochasticLut6<>::Create(64);
+    auto layer_sl4      = bb::StochasticLut6<>::Create(2048);
+    auto layer_sl5      = bb::StochasticLut6<>::Create(1024);
+    auto layer_sl6      = bb::StochasticLut6<>::Create(420);
+    auto layer_sl7      = bb::StochasticLut6<>::Create(70);
 
     {
         auto cnv0_sub = bb::Sequential::Create();
         cnv0_sub->Add(layer_cnv0_sl0);
+        cnv0_sub->Add(bb::BatchNormalization<>::Create());
         cnv0_sub->Add(layer_cnv0_sl1);
+        cnv0_sub->Add(bb::BatchNormalization<>::Create());
         cnv0_sub->Add(layer_cnv0_sl2);
+        cnv0_sub->Add(bb::BatchNormalization<>::Create());
 
         auto cnv1_sub = bb::Sequential::Create();
         cnv1_sub->Add(layer_cnv1_sl0);
+        cnv1_sub->Add(bb::BatchNormalization<>::Create());
         cnv1_sub->Add(layer_cnv1_sl1);
+        cnv1_sub->Add(bb::BatchNormalization<>::Create());
         cnv1_sub->Add(layer_cnv1_sl2);
+        cnv1_sub->Add(bb::BatchNormalization<>::Create());
 
         auto cnv2_sub = bb::Sequential::Create();
         cnv2_sub->Add(layer_cnv2_sl0);
+        cnv2_sub->Add(bb::BatchNormalization<>::Create());
         cnv2_sub->Add(layer_cnv2_sl1);
+        cnv2_sub->Add(bb::BatchNormalization<>::Create());
         cnv2_sub->Add(layer_cnv2_sl2);
+        cnv2_sub->Add(bb::BatchNormalization<>::Create());
 
         auto cnv3_sub = bb::Sequential::Create();
         cnv3_sub->Add(layer_cnv3_sl0);
+        cnv3_sub->Add(bb::BatchNormalization<>::Create());
         cnv3_sub->Add(layer_cnv3_sl1);
+        cnv3_sub->Add(bb::BatchNormalization<>::Create());
         cnv3_sub->Add(layer_cnv3_sl2);
+        cnv3_sub->Add(bb::BatchNormalization<>::Create());
         
         auto net = bb::Sequential::Create();
         net->Add(bb::LoweringConvolution<>::Create(cnv0_sub, 3, 3));
         net->Add(bb::LoweringConvolution<>::Create(cnv1_sub, 3, 3));
+//      net->Add(bb::StochasticMaxPooling2x2<>::Create());
         net->Add(bb::MaxPooling<>::Create(2, 2));
         net->Add(bb::LoweringConvolution<>::Create(cnv2_sub, 3, 3));
         net->Add(bb::LoweringConvolution<>::Create(cnv3_sub, 3, 3));
+//      net->Add(bb::StochasticMaxPooling2x2<>::Create());
         net->Add(bb::MaxPooling<>::Create(2, 2));
         net->Add(layer_sl4);
+        net->Add(bb::BatchNormalization<>::Create());
         net->Add(layer_sl5);
+        net->Add(bb::BatchNormalization<>::Create());
         net->Add(layer_sl6);
+        net->Add(bb::BatchNormalization<>::Create());
         net->Add(layer_sl7);
+        net->Add(bb::Reduce<>::Create(td.t_shape));
         net->SetInputShape(td.x_shape);
 
         if ( binary_mode ) {
             std::cout << "binary mode" << std::endl;
             net->SendCommand("binary true");
         }
+
+//      net->SendCommand("lut_binarize false");
+
+//      net->SendCommand("batch_normalization false");
+
+        net->SendCommand("fix_gamma true");
+        net->SendCommand("fix_beta  true");
+        net->SendCommand("set_gamma 0.2");
+        net->SendCommand("set_beta  0.5");
 
         // print model information
         net->PrintInfo();
@@ -116,11 +143,12 @@ void Cifar10Sparse6Cnn(int epoch_size, int mini_batch_size, int max_run_size, bo
         runner_create.lossFunc           = bb::LossSoftmaxCrossEntropy<float>::Create();
         runner_create.metricsFunc        = bb::MetricsCategoricalAccuracy<float>::Create();
         runner_create.optimizer          = bb::OptimizerAdam<float>::Create();
+//      runner_create.optimizer          = bb::OptimizerAdaGrad<float>::Create();
         runner_create.max_run_size       = max_run_size;    // 実際の1回の実行サイズ
         runner_create.file_read          = file_read;       // 前の計算結果があれば読み込んで再開するか
         runner_create.file_write         = true;            // 計算結果をファイルに保存するか
         runner_create.print_progress     = true;            // 途中結果を表示
-        runner_create.initial_evaluation = file_read;       // ファイルを読んだ場合は最初に評価しておく
+        runner_create.initial_evaluation = false; // file_read;       // ファイルを読んだ場合は最初に評価しておく
         auto runner = bb::Runner<float>::Create(runner_create);
         runner->Fitting(td, epoch_size, mini_batch_size);
     }
