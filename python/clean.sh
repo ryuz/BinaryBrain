5c97c85c64c2d7a86436ac8d469a047f4a8b8f86
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2019-09-15 18:00:34 +0900

    update pypi script

--- python/clean.sh
+++ python/clean.sh
@@ -4,3 +4,10 @@ rm -fr build
 rm -fr dist
 rm -fr binarybrain.egg-info
 rm -fr tmp
+
+rm -fr binarybrain/include
+rm -fr binarybrain/cuda
+
+rm -f  binarybrain/src/*.o
+rm -f  binarybrain/src/*.so
+
