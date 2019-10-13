741d9d224a9d153bf4cfef1a74559e8f5847d6ec
Ryuji Fuchikami <ryuz@users.sourceforge.jp>
2019-10-13 10:22:31 +0900

    rename Cifar10SparseLutCnnBit.py

--- python/samples/cifar10/Cifar10SparseLutCnnBit.py
+++ python/samples/cifar10/Cifar10SparseLutCnnBit.py
@@ -104,7 +104,7 @@ def main():
     optimizer.set_variables(net.get_parameters(), net.get_gradients())
     
     runner = bb.Runner(net, "cifar10-sparse-lut6-cnn", loss, metrics, optimizer)
-    runner.fitting(td, epoch_size=epoch, mini_batch_size=mini_batch)
+    runner.fitting(td, epoch_size=epoch, mini_batch_size=mini_batch, file_read=True, file_write=True)
     
     
 '''    
