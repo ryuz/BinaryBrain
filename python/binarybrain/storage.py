24a090e252897c81780804f6af9ece408b54bfe3
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2020-12-22 11:54:08 +0900

    update strage.py

--- python/binarybrain/storage.py
+++ python/binarybrain/storage.py
@@ -3,10 +3,50 @@
 import os
 import datetime
 import glob
+import re
+import shutil
 
 import binarybrain as bb
 
 
+def get_data_string():
+    return datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+
+def get_latest_path(path):
+    files = os.listdir(path)
+    dirs  = [f for f in files if os.path.isdir(os.path.join(path, f))]
+    
+    targets = []
+    for d in dirs:
+        if re.match('[0-9]{8}_[0-9]{6}', d):
+            targets.append(d)
+    
+    if not targets:
+        return None
+    
+    targets.sort(reverse=True)
+    return os.path.join(path, targets[0])
+
+
+def remove_old(path, keep=-1):
+    if keep < 0:
+        return
+    
+    files = os.listdir(path)
+    dirs  = [f for f in files if os.path.isdir(os.path.join(path, f))]
+    
+    targets = []
+    for d in dirs:
+        if re.match('[0-9]{8}_[0-9]{6}', d):
+            targets.append(d)
+    
+    targets.sort(reverse=True)
+    del targets[:keep]
+    
+    for t in targets:
+        shutil.rmtree(os.path.join(path, t))
+
+    
 def get_model_list(net, flatten=False):
     if  type(net) is not list:
         net = [net]
@@ -43,20 +83,20 @@ def save_networls(path, net):
         
         file_path = os.path.join(path, fname)
         
+        print(file_path)
         with open(file_path, 'wb') as f:
             f.write(model.dump_bytes())
 
-def save(path: str, net):
+def save(path: str, net, keep_olds=-1):
     models = get_model_list(net, flatten=True)
     
     os.makedirs(path, exist_ok=True)
     
-    files = os.listdir(path)
-    dirs  = [f for f in files if os.path.isdir(os.path.join(path, f))]
-    
-    date_str = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    date_str = get_data_string()
     data_path = os.path.join(path, date_str)
     os.makedirs(data_path, exist_ok=True)
     
-    save_networls(path, net)
-
+    save_networls(data_path, net)
+    
+    if keep_olds >= 0:
+        remove_old(path, keep=keep_olds)
