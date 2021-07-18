febceea5536c19f4f32d7cfe1603aa3294b968b4
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2021-07-18 19:51:11 +0900

    update

--- samples/cpp/mnist/MnistAeDifferentiableLutCnn.cpp
+++ samples/cpp/mnist/MnistAeDifferentiableLutCnn.cpp
@@ -187,7 +187,7 @@ void MnistAeDifferentiableLutCnn_Tmp(int epoch_size, int mini_batch_size, int tr
 
 
         // write pgm
-        bb::FrameBuffer x_buf(8, {28, 28, 1}, BB_TYPE_FP32);
+        bb::FrameBuffer x_buf(8, {1, 28, 28}, BB_TYPE_FP32);
         x_buf.SetVector(td.x_test, 0);
         auto y_buf = net->Forward(x_buf, false);
         WritePgm("out_0x.pgm", x_buf, 0);
@@ -342,7 +342,7 @@ void MnistAeDifferentiableLutCnn_Tmp(int epoch_size, int mini_batch_size, int tr
             std::cout << "lut_accuracy : " << lut_accuracy << std::endl;
 
             // write pgm
-            bb::FrameBuffer x_buf(32, {28, 28, 1}, BB_TYPE_FP32);
+            bb::FrameBuffer x_buf(32, {1, 28, 28}, BB_TYPE_FP32);
             x_buf.SetVector(td.x_test, 0);
             auto y_buf = eval_net->Forward(x_buf, false);
             WritePgm("lut_0x.pgm", x_buf, 0);
